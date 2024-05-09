57aefe846d7b0f5fd0ff774b91bba5f4f561aacb
Amy Zinger <amy.zinger@gmail.com>
2024-05-08 23:55:23 -0500

    updated site with new images and info

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,9 @@
-"use strict";
+(function () {
+  "use strict";
 
-( () =>{
+  // document.querySelector("#testButton").addEventListener("click", handleClick);
 
-document.querySelector("#testButton").addEventListener("click", handleClick);
-
-function handleClick() {
-  console.log("Thank you for clicking.");
-}
-
+  // function handleClick() {
+  //   console.log("Thank you for clicking.");
+  // }
 })();
