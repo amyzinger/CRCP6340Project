1c52518df4fbe8905e6f9ec8ed8f50b0379977b4
Amy Zinger <amy.zinger@gmail.com>
2024-05-14 22:29:34 -0500

    added contact form back end

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,7 @@
 (function () {
   "use strict";
 
-  // document.querySelector("#testButton").addEventListener("click", handleClick);
 
-  // function handleClick() {
-  //   console.log("Thank you for clicking.");
-  // }
+
+
 })();
