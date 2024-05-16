0711cd6c7d29935b2fbc8aa3b5908934f25a7eda
Amy Zinger <amy.zinger@gmail.com>
2024-05-15 20:46:22 -0500

    increased form functionality

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,18 +1,35 @@
 (function () {
     "use strict";
+
+      
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let form = document.querySelector("#contact-form");
+
+    document.querySelector("#send-contact").addEventListener("click", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      let formValid = true;
+      if (!form.checkValidity()) {
+        formValid = false;
+      }
+      form.classList.add("was-validated");
+      if (formValid) {
+        sendTheEmail();
+      }
+    });
+
   
-    document.querySelector("#contact-form-button")
-        .addEventListener("click", (event) => {
-        event.preventDefault();
-        event.stopPropagation();
+      function sendTheEmail(){
         console.log("You clicked the submit button.");
-        let name = document.querySelector("#name").value;
+        let firstName = document.querySelector("#first-name").value;
+        let lastName = document.querySelector("#last-name").value;
         let email = document.querySelector("#mail").value;
         let message = document.querySelector("#msg").value;
-        console.log("Name: " + name);
+        console.log("First name: " + firstName);
+        console.log("Last name: " + lastName);
         console.log("Email: " + email);
         console.log("Message: " + message);
-      });
+      }
   
   
   })();
