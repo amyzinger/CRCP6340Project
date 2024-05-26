4dd5020cbe05d56e660c4bccd914808179744298
Amy Zinger <amy.zinger@gmail.com>
2024-05-25 20:53:16 -0500

    incorporated ejs including navigation and footer

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs", {projectArray: data});
+});
+
+app.get("/newProject", (req, res) => {
+  res.render("newProject.ejs");
+});
+
+app.get("/project", (req, res) => {
+  res.render("project.ejs");
+});
 
 app.post("/mail", async (req, res) => {
   await utils
