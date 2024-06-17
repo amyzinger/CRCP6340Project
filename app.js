99b25923f4c266f7e62588687e7f50a20ec95268
Amy Zinger <amy.zinger@gmail.com>
2024-06-16 22:04:36 -0500

    changed app.js to render projects and index pages with common handler function, updated .ejs files appropriately

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
 import * as db from "./utils/database.js";
-let data = ["Project 1", "Project 2", "Project 3"];
-let projects = [];
 
 const app = express();
 const port = 3000;
@@ -12,58 +10,64 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", async (req, res, next) => {
-  await db
-    .connect()
-    .then(async () => {
-      // query the database for project records
-      projects = await db.getAllProjects();
-      console.log(projects);
-      // Pass the first three projects to the template
-      res.render("index.ejs", { projects: projects.slice(0, 3) });
-    })
-    .catch(next);
-});
-
-app.get("/projects", (req, res) => {
-  res.render("projects.ejs", { projectArray: projects });
-});
-
-app.get("/project/:id", (req, res) => {
-  let id = req.params.id;
-  if (id > data.length) {
-    throw new Error("No project with that ID");
+// Common handler function to render pages with projects data
+async function renderProjectsPage(req, res, next, template, limit = null) {
+  try {
+    await db.connect();
+    let projects = await db.getAllProjects();
+    if (limit) {
+      projects = projects.slice(0, limit);
+    }
+    res.render(template, { projectArray: projects });
+  } catch (error) {
+    next(error);
   }
-  res.render("project.ejs", { project: projects[id - 1], which: id });
+}
+
+// Route for the index page
+app.get("/", (req, res, next) => {
+  renderProjectsPage(req, res, next, "index.ejs", 3);
 });
 
+// Route for the projects page
+app.get("/projects", (req, res, next) => {
+  renderProjectsPage(req, res, next, "projects.ejs");
+});
+
+// Route for individual project pages
+app.get("/project/:id", (req, res, next) => {
+  let id = parseInt(req.params.id, 10);
+  if (id > 0 && id <= projects.length) {
+    res.render("project.ejs", { project: projects[id - 1], which: id });
+  } else {
+    next(new Error("No project with that ID"));
+  }
+});
+
+// Route for the about page
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
 
+// Route for the contact page
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
 });
 
+// Route for sending mail
 app.post("/mail", async (req, res) => {
-  await utils
-    .sendMessage(req.body.sub, req.body.txt)
-    .then(() => {
-      res.send({ result: "success" });
-    })
-    .catch(() => {
-      res.send({ result: "failure" });
-    });
+  try {
+    await utils.sendMessage(req.body.sub, req.body.txt);
+    res.send({ result: "success" });
+  } catch {
+    res.send({ result: "failure" });
+  }
 });
 
-app.use(async (err, req, res, next) => {
+// Error handling middleware
+app.use((err, req, res, next) => {
   console.log(err);
-  let msg;
-  msg = err.message;
-  if (msg != "No project with that ID") {
-    msg =
-      "Oops! There was an internal error.";
-  }
+  let msg = err.message !== "No project with that ID" ? "Oops! There was an internal error." : err.message;
   res.render("error.ejs", { msg: msg });
 });
 
